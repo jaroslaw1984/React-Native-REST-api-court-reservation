b68860af02ee9d7109f30147953b1ffd215f655d
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-06-26 17:56:38 +0200

    Adding Register and Login components to the routes

--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -1,16 +1,25 @@
 import React from "react";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createStackNavigator, HeaderTitle } from "@react-navigation/stack";
 import { StyleSheet, View, SafeAreaView, Text } from "react-native";
+import { WebView } from "react-native-webview";
 import Login from "../screens/login/Login";
 
-const LoginScreen = () => {
+const LoginScreen = ({ navigation }) => {
   return (
     <View>
-      <Login />
+      <Login nav={navigation} />
     </View>
   );
+};
+
+const Register = () => {
+  return <WebView source={{ uri: "https://korty.org/rejestracja" }} />;
+};
+
+const Recovery = () => {
+  return <WebView source={{ uri: "https://korty.org/przypomnij-haslo" }} />;
 };
 
 const Stack = createStackNavigator();
@@ -18,8 +27,22 @@ const Stack = createStackNavigator();
 const Routes = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ header: () => null }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={Register}
+          options={{ headerTitle: "Stwórz konto" }}
+        />
+        <Stack.Screen
+          name="Recovery"
+          component={Recovery}
+          options={{ headerTitle: "Przypomnij hasło" }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
