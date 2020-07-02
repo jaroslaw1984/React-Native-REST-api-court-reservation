02e4a2c624e3b1980e2ed27b845dfe773e72ce38
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-07-02 11:06:05 +0200

    Unused modules are removed from import react-native

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "react-native-gesture-handler";
-import { StyleSheet, View, SafeAreaView, Text } from "react-native";
+import { StyleSheet, SafeAreaView } from "react-native";
 import Routes from "./src/components/routes/Routes";
 import { UserProvider } from "./src/components/context/UserProvider";
 
