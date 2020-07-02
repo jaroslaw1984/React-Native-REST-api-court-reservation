eadd4a68f20b3d72d2cf380254cf5c75205ccabb
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-07-02 11:11:50 +0200

    Adding values for UserContex.Provider

--- src/components/context/UserProvider.js
+++ src/components/context/UserProvider.js
@@ -1,16 +1,31 @@
 import React, { useContext, useState, createContext } from "react";
+import { AsyncStorage } from "react-native";
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const [login, setLogin] = useState(null);
+  const [user, setUser] = useState(null);
 
   const setDataContext = (data) => {
-    setLogin(data);
+    setUser(data);
   };
 
   return (
-    <UserContext.Provider value={{ login, setDataContext }}>
+    <UserContext.Provider
+      value={{
+        user,
+        setDataContext,
+        login: () => {
+          const logetUser = user.config.data._parts[0][1];
+          setUser(logetUser);
+          AsyncStorage.setItem("user", logetUser);
+        },
+        logout: () => {
+          setUser(null);
+          AsyncStorage.removeItem("user");
+        },
+      }}
+    >
       {children}
     </UserContext.Provider>
   );
