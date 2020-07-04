7e2368a17241f3b40347df98ac9fc9bb8b0b5eff
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-07-04 14:03:42 +0200

    Created club components

---
+++ src/components/screens/clubs/ClubList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const ClubList = () => {
+  return (
+    <View>
+      <Text>Lista wszystkich klubów</Text>
+    </View>
+  );
+};
+
+export default ClubList;
---
+++ src/components/screens/clubs/Clubs.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import {
+  StyleSheet,
+  View,
+  Text,
+  StatusBar,
+  Platform,
+  Button,
+} from "react-native";
+import { UserContext } from "../../context/UserProvider";
+
+const Clubs = ({ nav }) => {
+  const { user } = useContext(UserContext);
+  const { userClubs, setUserClubs } = useState([]);
+
+  useEffect(() => {
+    const fetchClubs = async () => {
+      const url = "https://korty.org/api/clubs/bookmark/show";
+
+      const userSessionKey = new FormData();
+
+      userSessionKey.append("session_key", user.data.results.session_key);
+
+      await axios
+        .post(url, userSessionKey)
+        .then((respond) => {
+          setUserClubs(respond.data.results);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    };
+    fetchClubs();
+  }, []);
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Lista klubów</Text>
+      <Button
+        title="Zobacz wszystkie kluby"
+        onPress={() => nav.navigate("Lista")}
+      />
+    </View>
+  );
+};
+
+export default Clubs;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+});
