87ace795da70c510c09e8b4304e97f635ef3a97f
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-07-04 14:04:46 +0200

    Deleted unused methods form react-native module

--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -2,16 +2,7 @@ import React, { useContext, useState, useEffect } from "react";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 
-import {
-  StyleSheet,
-  View,
-  SafeAreaView,
-  Text,
-  ActivityIndicator,
-  AsyncStorage,
-} from "react-native";
-import { WebView } from "react-native-webview";
-import Login from "../screens/login/Login";
+import { StyleSheet, ActivityIndicator, AsyncStorage } from "react-native";
 import { UserContext } from "../context/UserProvider";
 import UserMenu from "../screens/userMenu/UserMenu";
 import StackNavigator from "../screens/stackNavigator/StackNavigator";
