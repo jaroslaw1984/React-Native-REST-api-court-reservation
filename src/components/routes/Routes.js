a926cc82ec925150cb8c93bff5cd74173eb2837b
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-07-06 18:45:32 +0200

    Empty space has been removed

--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -1,7 +1,6 @@
 import React, { useContext, useState, useEffect } from "react";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
-
 import { StyleSheet, ActivityIndicator, AsyncStorage } from "react-native";
 import { UserContext } from "../context/UserProvider";
 import UserMenu from "../screens/userMenu/UserMenu";
