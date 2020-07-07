3a7a924962ffabac9b0c0597f8b1f8f269984ed4
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-07-07 21:31:49 +0200

    Refactoring the component

--- src/components/screens/clubs/Clubs.jsx
+++ src/components/screens/clubs/Clubs.jsx
@@ -1,17 +1,8 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
-import {
-  StyleSheet,
-  View,
-  Text,
-  StatusBar,
-  Platform,
-  Button,
-  FlatList,
-  Image,
-  ActivityIndicator,
-} from "react-native";
+import { StyleSheet, ActivityIndicator } from "react-native";
 import { UserContext } from "../../context/UserProvider";
+import ClubItem from "./ClubItem";
 
 const useClubs = (url) => {
   const { user } = useContext(UserContext);
@@ -43,152 +34,14 @@ const Clubs = ({ nav }) => {
   if (!data) {
     return <ActivityIndicator style={styles.loading_Indicator} size="large" />;
   } else {
-    return (
-      <View style={styles.club__container}>
-        <FlatList
-          keyExtractor={(item) => item.id.toString()}
-          data={data}
-          renderItem={({ item }) => (
-            <View style={styles.club__item}>
-              <View
-                style={
-                  item.status === 1
-                    ? styles.club__container__img
-                    : styles.club__container__img__offline
-                }
-              >
-                <Image
-                  source={
-                    item.logo_src === ""
-                      ? require("../../../../assets/ic_launcher_foreground.png")
-                      : { uri: item.logo_src }
-                  }
-                  style={styles.club__img}
-                />
-              </View>
-              <View
-                style={
-                  item.status === 1
-                    ? styles.club__info
-                    : styles.club__info__offline
-                }
-              >
-                <Text style={styles.club__name}>{item.name}</Text>
-                <Text>
-                  {item.city_name} ({item.district_name})
-                </Text>
-                <Text>{item.address}</Text>
-              </View>
-              <View style={styles.club__status}>
-                <View
-                  style={
-                    item.status === 1
-                      ? styles.club__isOnline
-                      : styles.club__isOffline
-                  }
-                ></View>
-                <Text>{item.status === 1 ? "Online" : "Offline"}</Text>
-              </View>
-            </View>
-          )}
-        />
-        <Button
-          title="Zobacz wszystkie kluby"
-          onPress={() => nav.navigate("Lista")}
-        />
-      </View>
-    );
+    return <ClubItem nav={nav} data={data} />;
   }
 };
 export default Clubs;
 
 const styles = StyleSheet.create({
-  club__container: {
-    justifyContent: "flex-start",
-    paddingTop: 10,
-    paddingHorizontal: 10,
-  },
-  club__item: {
-    flex: 1,
-    width: "100%",
-    flexDirection: "row",
-    paddingBottom: 20,
-  },
-  club__container__img: {
-    borderWidth: 2,
-    borderRadius: 50,
-    borderColor: "#62929a",
-    overflow: "hidden",
-  },
-  club__container__img__offline: {
-    borderWidth: 2,
-    borderRadius: 50,
-    overflow: "hidden",
-    opacity: 0.3,
-  },
-  club__img: {
-    width: 80,
-    height: 80,
-  },
-  club__info: {
-    flex: 2,
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
-    paddingHorizontal: 10,
-  },
-  club__info__offline: {
-    flex: 2,
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
-    paddingHorizontal: 10,
-    opacity: 0.2,
-  },
   loading_Indicator: {
     flex: 1,
     justifyContent: "center",
-  },
-  club__status: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  club__isOnline: {
-    width: 30,
-    height: 30,
-    borderWidth: 2,
-    borderRadius: 50,
-    borderColor: "#dee1ec",
-    backgroundColor: "#2cb978",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 4,
-    },
-    shadowOpacity: 0.3,
-    shadowRadius: 4.65,
-    elevation: 8,
-  },
-  club__isOffline: {
-    width: 30,
-    height: 30,
-    borderWidth: 2,
-    borderRadius: 50,
-    borderColor: "#dee1ec",
-    backgroundColor: "#d72323",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 4,
-    },
-    shadowOpacity: 0.3,
-    shadowRadius: 4.65,
-    elevation: 8,
-  },
-  club__name: {
-    fontSize: 18,
-    fontWeight: "bold",
   },
 });
