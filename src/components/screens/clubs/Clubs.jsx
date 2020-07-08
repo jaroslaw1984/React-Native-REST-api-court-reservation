1aaa8973a8d24b2353612644c0579521971cf44e
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-07-08 21:23:50 +0200

    SetTimeout has been added to setState and refactoring the code

--- src/components/screens/clubs/Clubs.jsx
+++ src/components/screens/clubs/Clubs.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
-import { StyleSheet, ActivityIndicator } from "react-native";
+import { StyleSheet, ActivityIndicator, View } from "react-native";
 import { UserContext } from "../../context/UserProvider";
 import ClubItem from "./ClubItem";
 
 const useClubs = (url) => {
   const { user } = useContext(UserContext);
   const [userClubs, setUserClubs] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const fetchClubs = async () => {
     const userSessionKey = new FormData();
@@ -15,7 +16,12 @@ const useClubs = (url) => {
 
     await axios
       .post(url, userSessionKey)
-      .then((respond) => setUserClubs(respond.data.results))
+      .then((respond) => {
+        setTimeout(() => {
+          setUserClubs(respond.data.results);
+          setLoading(false);
+        }, 1000);
+      })
       .catch((err) => {
         console.error(err);
       });
@@ -25,23 +31,38 @@ const useClubs = (url) => {
     fetchClubs();
   }, [url]);
 
-  return userClubs;
+  return { userClubs, loading };
 };
 
 const Clubs = ({ nav }) => {
-  const data = useClubs("https://korty.org/api/clubs/bookmark/show");
+  const { userClubs, loading } = useClubs(
+    "https://korty.org/api/clubs/bookmark/show"
+  );
 
-  if (!data) {
-    return <ActivityIndicator style={styles.loading_Indicator} size="large" />;
-  } else {
-    return <ClubItem nav={nav} data={data} />;
-  }
+  return (
+    <View style={styles.club__container}>
+      {loading ? (
+        <ActivityIndicator size="large" />
+      ) : (
+        <ClubItem nav={nav} data={userClubs} />
+      )}
+    </View>
+  );
 };
+
 export default Clubs;
 
 const styles = StyleSheet.create({
+  club__container: {
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center",
+    paddingTop: 10,
+    paddingHorizontal: 10,
+  },
   loading_Indicator: {
     flex: 1,
     justifyContent: "center",
+    alignItems: "center",
   },
 });
