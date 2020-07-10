54e3d24cdd124e98dc237f445159b6401d0ed0da
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-07-10 21:20:08 +0200

    ActivityIndicator as loading icon has been removed

--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -1,13 +1,12 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
-import { StyleSheet, ActivityIndicator, AsyncStorage } from "react-native";
+import { AsyncStorage } from "react-native";
 import { UserContext } from "../context/UserProvider";
 import UserMenu from "../screens/userMenu/UserMenu";
 import StackNavigator from "../screens/stackNavigator/StackNavigator";
 
 const Routes = () => {
-  const [loading, setLoading] = useState(true);
   const { user, login } = useContext(UserContext);
 
   useEffect(() => {
@@ -17,28 +16,17 @@ const Routes = () => {
         if (item) {
           login();
         }
-        setLoading(false);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
-  if (loading) {
-    return <ActivityIndicator style={styles.loading_Indicator} size="large" />;
-  }
   return (
     <NavigationContainer>
       {user ? <UserMenu /> : <StackNavigator />}
     </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  loading_Indicator: {
-    flex: 1,
-    justifyContent: "center",
-  },
-});
 
 export default Routes;
