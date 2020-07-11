fa5a7e07880dfac2624bb07c33a523b7c1341145
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-07-11 11:59:58 +0200

    Creating component ClubCard that display a single card

---
+++ src/components/screens/clubs/ClubCard.jsx
@@ -0,0 +1,232 @@
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
+const ClubCard = ({ nav, data }) => {
+  return (
+    <FlatList
+      keyExtractor={(item) => item.id.toString()}
+      data={data}
+      numColumns={2}
+      renderItem={({ item }) => (
+        <View style={styles.club__card}>
+          <TouchableOpacity
+            onPress={() =>
+              nav.navigate("Informacje o klubie", { url: item.url })
+            }
+          >
+            <View style={styles.club__item}>
+              <View
+                style={
+                  item.status === 1
+                    ? styles.club__container__img
+                    : styles.club__container__img__offline
+                }
+              >
+                <Image
+                  source={
+                    item.logo_src === ""
+                      ? require("../../../../assets/ic_launcher_foreground.png")
+                      : { uri: item.logo_src }
+                  }
+                  style={styles.club__img}
+                />
+                <View style={styles.card__settings}>
+                  <MaterialIcons name="settings" size={40} color="white" />
+                </View>
+                <View style={styles.img__bg__circle}></View>
+              </View>
+              <View
+                style={
+                  item.status === 1
+                    ? styles.club__info
+                    : styles.club__info__offline
+                }
+              >
+                <Text style={styles.club__name}>{item.name}</Text>
+                {item.district_name && (
+                  <Text
+                    style={styles.club__district}
+                  >{`(${item.district_name})`}</Text>
+                )}
+                <Text style={styles.club__address}>{item.address}</Text>
+              </View>
+              <View style={styles.club__status}>
+                <View
+                  style={
+                    item.status === 1
+                      ? styles.club__isOnline
+                      : styles.club__isOffline
+                  }
+                ></View>
+                <Text style={styles.club__status__text}>
+                  {item.status === 1 ? "Online" : "Offline"}
+                </Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        </View>
+      )}
+    />
+  );
+};
+
+export default ClubCard;
+
+const styles = StyleSheet.create({
+  club__card: {
+    flex: 1,
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+  club__item: {
+    // all card will have the same height if flex: 1 is on
+    flex: 1,
+    width: 190,
+    alignItems: "center",
+    paddingVertical: 10,
+    marginVertical: 10,
+    backgroundColor: "#dee1ec",
+    // borderColor: "#2470a0",
+    // borderWidth: 2,
+    borderRadius: 5,
+    shadowColor: "#000",
+    overflow: "hidden",
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.37,
+    shadowRadius: 7.49,
+
+    elevation: 12,
+  },
+  club__container__img: {
+    flex: 1,
+    justifyContent: "center",
+    alignSelf: "flex-start",
+    marginLeft: 20,
+    marginVertical: 50,
+  },
+  card__settings: {
+    position: "absolute",
+    top: -50,
+    right: -65,
+  },
+  img__bg__circle: {
+    position: "absolute",
+    top: -170,
+    left: -75,
+    width: 250,
+    height: 250,
+    borderRadius: 100,
+    backgroundColor: "#6740FF",
+    zIndex: -1,
+  },
+  club__container__img__offline: {
+    flex: 1,
+    justifyContent: "center",
+    alignSelf: "flex-start",
+    marginLeft: 20,
+    marginVertical: 50,
+    opacity: 0.3,
+  },
+  club__img: {
+    width: 90,
+    height: 90,
+    borderRadius: 10,
+  },
+  club__info: {
+    flex: 1,
+    alignSelf: "stretch",
+    textAlign: "center",
+    flexDirection: "column",
+    padding: 4,
+    paddingVertical: 20,
+  },
+  club__name: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    lineHeight: 25,
+  },
+  club__district: {
+    fontSize: 15,
+    paddingTop: 5,
+    textAlign: "center",
+    opacity: 0.5,
+  },
+  club__address: {
+    fontSize: 15,
+    textAlign: "center",
+    opacity: 0.5,
+  },
+  club__info__offline: {
+    flex: 1,
+    alignSelf: "stretch",
+    textAlign: "center",
+    flexDirection: "column",
+    padding: 8,
+    paddingVertical: 20,
+    marginVertical: 20,
+    opacity: 0.2,
+  },
+  loading_Indicator: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  club__status: {
+    flex: 1,
+    alignSelf: "stretch",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    borderTopColor: "#516c8d",
+    borderTopWidth: 1,
+    paddingTop: 10,
+  },
+  club__status__text: {
+    fontWeight: "bold",
+  },
+  club__isOnline: {
+    width: 30,
+    height: 30,
+    borderWidth: 2,
+    borderRadius: 50,
+    borderColor: "#dee1ec",
+    backgroundColor: "#2cb978",
+    marginRight: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+    elevation: 8,
+  },
+  club__isOffline: {
+    width: 30,
+    height: 30,
+    borderWidth: 2,
+    borderRadius: 50,
+    borderColor: "#dee1ec",
+    backgroundColor: "#d72323",
+    marginRight: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+    elevation: 8,
+  },
+});
