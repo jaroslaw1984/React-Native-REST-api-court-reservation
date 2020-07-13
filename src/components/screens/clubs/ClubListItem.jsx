26f98c98e4b99a986913d15deec808fd60127205
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-07-13 17:42:50 +0200

    FlatList has been added

--- src/components/screens/clubs/ClubListItem.jsx
+++ src/components/screens/clubs/ClubListItem.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import ClubCard from "./ClubCard";
+import { FlatList } from "react-native";
 
 const ClubListItem = ({ nav, data }) => {
-  return <ClubCard nav={nav} data={data} />;
+  return (
+    <FlatList
+      keyExtractor={(item) => item.id}
+      data={data}
+      numColumns={2}
+      renderItem={({ item }) => (
+        <ClubCard nav={nav} item={item} key={item.id} />
+      )}
+    />
+  );
 };
 
 export default ClubListItem;
