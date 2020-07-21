9619203d7bfe9d08d3cfadbb9ea6a44c1d08c67b
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-07-21 08:10:01 +0200

    Creating RootDrawer component for drawer navigation

---
+++ src/components/routes/RootDrawer.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Button, View } from "react-native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button
+        onPress={() => navigation.navigate("Notifications")}
+        title="Go to notifications"
+      />
+    </View>
+  );
+};
+
+const NotificationsScreen = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+};
+
+const Drawer = createDrawerNavigator();
+
+const RootDrawer = () => {
+  return (
+    <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+    </Drawer.Navigator>
+  );
+};
+
+export default RootDrawer;
