ef71efb3f9cf7a573227f135684d688ab3df220f
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-07-31 20:31:37 +0200

    Unused methods has been removed and session key function added

--- src/components/screens/userMenu/UserMenu.jsx
+++ src/components/screens/userMenu/UserMenu.jsx
@@ -1,18 +1,26 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useContext, useState } from "react";
+import { View } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createStackNavigator } from "@react-navigation/stack";
+import { WebView } from "react-native-webview";
 import { FontAwesome } from "@expo/vector-icons";
 import TopMenu from "./TopMenu";
 import Clubs from "../clubs/Clubs";
 import Reservations from "../Reservations";
 import ClubList from "../clubs/ClubList";
-import { createStackNavigator } from "@react-navigation/stack";
-import { WebView } from "react-native-webview";
 import RootDrawer from "../../routes/RootDrawer";
+import UserContext from "../../context/UserProvider";
+
+const setCookie = ({ user }) => {
+  const newCookie = user.data.results.session_key;
+
+  document.cookie = `session_key_api=${newCookie}; expires=Tue, 25-Aug-2020 10:39:25 GMT; Max-Age=2678400; path=/`;
+};
 
 // This show details about club and load the webpage of club info
 const AboutClub = ({ route }) => {
+  // const sessionKey = user.data.results.session_key;
+
   const { url } = route.params;
   return <WebView source={{ uri: url }} />;
 };
@@ -90,20 +98,10 @@ const BottomTabStack = () => {
   );
 };
 
-const Drawer = createDrawerNavigator();
-
 // Drawer left menu
-const UserMenu = () => {
+const UserMenu = ({ user }) => {
+  console.log(user.data.results.session_key);
   return <RootDrawer bottomStack={BottomTabStack} />;
 };
-
-const styles = StyleSheet.create({
-  reservation_btn: {
-    // flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    textAlign: "center",
-  },
-});
 
 export default UserMenu;
