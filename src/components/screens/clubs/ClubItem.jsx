fffd1a536e1d7b424647e0780d7d5172f0900aa8
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-08-05 19:50:09 +0200

    Added function that allow to add or delete club from API and implementing ClubAlertModal. Animaton form modal has been added

--- src/components/screens/clubs/ClubItem.jsx
+++ src/components/screens/clubs/ClubItem.jsx
@@ -1,9 +1,70 @@
-import React from "react";
-import { StyleSheet, View, Text, FlatList } from "react-native";
+import React, { useState, useContext, useRef } from "react";
+import { StyleSheet, View, Text, FlatList, Animated } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import ClubCard from "./ClubCard";
+import ClubAlertModal from "./ClubAlertModal";
+import { UserContext } from "../../context/UserProvider";
+import axios from "axios";
 
-const ClubItem = ({ nav, data }) => {
+const ClubItem = ({ nav, data, onRemove }) => {
+  const [modalActive, setModalActive] = useState(false);
+  const [clubId, setClubId] = useState(Number);
+  const [clubName, setClubName] = useState(String);
+  const { user } = useContext(UserContext);
+
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  const handleOpenModal = () => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+    setModalActive(true);
+  };
+
+  const handleCloseModal = () => {
+    Animated.timing(fadeAnim, {
+      toValue: 0,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+    setTimeout(() => {
+      setModalActive(false);
+    }, 500);
+  };
+
+  const getClubId = (id) => {
+    setClubId(id);
+  };
+
+  const getClubName = (name) => {
+    setClubName(name);
+  };
+
+  // this function allow to add or delete club from API depends what attribute were pass in setOption
+  const useFavoriteOptions = async (setOption, cardId) => {
+    const id = Number.isInteger(cardId);
+    const option = typeof setOption === "string";
+    const cardDetails = new FormData();
+
+    cardDetails.append("session_key", user.data.results.session_key);
+    cardDetails.append("club_id", id ? cardId : null);
+
+    if (setOption === "remove") {
+      await axios
+        .post(
+          `https://korty.org/api/clubs/bookmark/${option ? setOption : null}`,
+          cardDetails
+        )
+        .catch((err) => {
+          console.error(err);
+        });
+
+      onRemove(cardId);
+    }
+  };
+
   return (
     <React.Fragment>
       {/* this condition need to be here for button at the bottom */}
@@ -14,7 +75,15 @@ const ClubItem = ({ nav, data }) => {
           keyExtractor={(item) => item.id.toString()}
           data={data}
           renderItem={({ item }) => (
-            <ClubCard nav={nav} item={item} key={item.id} />
+            <ClubCard
+              nav={nav}
+              item={item}
+              key={item.id}
+              onRemove={onRemove}
+              onModal={handleOpenModal}
+              clubId={getClubId}
+              clubName={getClubName}
+            />
           )}
         />
       )}
@@ -29,6 +98,23 @@ const ClubItem = ({ nav, data }) => {
           />
         </View>
       </View>
+      {modalActive && (
+        <Animated.View
+          style={[
+            styles.alertModal,
+            {
+              opacity: fadeAnim,
+            },
+          ]}
+        >
+          <ClubAlertModal
+            onDelete={useFavoriteOptions}
+            onClose={handleCloseModal}
+            clubId={clubId}
+            clubName={clubName}
+          />
+        </Animated.View>
+      )}
     </React.Fragment>
   );
 };
@@ -50,5 +136,15 @@ const styles = StyleSheet.create({
   },
   button__icon: {
     color: "#fff",
+  },
+  alertModal: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(87, 81, 81, 0.3)",
   },
 });
