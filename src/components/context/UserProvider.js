3f0e1a5fe168787205e3345fd2cd02e019751bfd
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-08-07 21:39:02 +0200

    Moved most function and logic to context

--- src/components/context/UserProvider.js
+++ src/components/context/UserProvider.js
@@ -1,13 +1,41 @@
-import React, { useContext, useState, createContext } from "react";
+import React, { useState, createContext } from "react";
 import { AsyncStorage } from "react-native";
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [userClubs, setUserClubs] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [clubList, setClubList] = useState(false);
 
   const setDataContext = (data) => {
     setUser(data);
+  };
+
+  const handleUserClubs = (data) => {
+    setUserClubs(data);
+  };
+
+  // it will check if user clicked on club list
+  const clubListScreenActive = () => {
+    setClubList(true);
+  };
+
+  // add club to favorite
+  const handleAddClub = (club) => {
+    setUserClubs((userClubs) => [...userClubs, club]);
+  };
+
+  const handleLoading = () => {
+    setLoading(false);
+  };
+
+  // this function remove selected card from DOM
+  const handleRemoveClub = (id) => {
+    const newList = userClubs.filter((item) => item.id !== id);
+
+    setUserClubs(newList);
   };
 
   return (
@@ -15,6 +43,14 @@ export const UserProvider = ({ children }) => {
       value={{
         user,
         setDataContext,
+        userClubs,
+        clubListScreenActive,
+        clubList,
+        handleAddClub,
+        handleUserClubs,
+        handleRemoveClub,
+        loading,
+        handleLoading,
         login: () => {
           const logetUser = user;
           setUser(logetUser);
