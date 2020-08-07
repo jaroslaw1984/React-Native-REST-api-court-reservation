5863dd428f00f3e7de3804e29445198fba05c09c
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-08-07 21:49:29 +0200

    Adding function that allow to add club to favorite screen

--- src/components/screens/clubs/ClubListItem.jsx
+++ src/components/screens/clubs/ClubListItem.jsx
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import ClubCard from "./ClubCard";
 import { FlatList } from "react-native";
+import { UserContext } from "../../context/UserProvider";
+import axios from "axios";
 
 const ClubListItem = ({ nav, data }) => {
+  const [clubId, setClubId] = useState(Number);
+  const { user, clubList, handleAddClub } = useContext(UserContext);
+
+  const useFavoriteOptions = async (setOption, cardId) => {
+    const id = Number.isInteger(cardId);
+    const option = typeof setOption === "string";
+    const cardDetails = new FormData();
+
+    cardDetails.append("session_key", user.data.results.session_key);
+    cardDetails.append("club_id", id ? cardId : null);
+
+    if (setOption === "add") {
+      await axios
+        .post(
+          `https://korty.org/api/clubs/bookmark/${option ? setOption : null}`,
+          cardDetails
+        )
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  };
+
+  // get club id
+  const getClubId = (id) => {
+    setClubId(id);
+  };
+
   return (
     <FlatList
       keyExtractor={(item) => item.id.toString()}
       data={data}
-      renderItem={({ item }) => (
-        <ClubCard nav={nav} item={item} key={item.id} />
+      renderItem={({ item, index }) => (
+        <ClubCard
+          nav={nav}
+          item={item}
+          index={index}
+          key={item.id}
+          clubId={getClubId}
+          clubListActive={clubList}
+          onAdd={useFavoriteOptions}
+          addClub={handleAddClub}
+        />
       )}
     />
   );
