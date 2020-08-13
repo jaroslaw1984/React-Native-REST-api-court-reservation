1a56834bb8f2a3d0d7d273ba7733ad597639db1a
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-08-13 10:07:14 +0200

    Bug fixed with touchable button range

--- src/components/screens/clubs/ClubItem.jsx
+++ src/components/screens/clubs/ClubItem.jsx
@@ -1,76 +1,17 @@
-import React, { useState, useContext, useRef } from "react";
-import { StyleSheet, View, Text, FlatList, Animated } from "react-native";
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import ClubCard from "./ClubCard";
-import ClubAlertModal from "./ClubAlertModal";
-import { UserContext } from "../../context/UserProvider";
-import axios from "axios";
 
-const ClubItem = ({ nav, data, onRemove, onAdd }) => {
-  const [modalActive, setModalActive] = useState(false);
-  const [clubId, setClubId] = useState(Number);
-  const [clubName, setClubName] = useState(String);
-
-  const { user, clubListScreenActive } = useContext(UserContext);
-
-  const fadeAnim = useRef(new Animated.Value(0)).current;
-
-  const handleOpenModal = () => {
-    Animated.timing(fadeAnim, {
-      toValue: 1,
-      duration: 500,
-      useNativeDriver: true,
-    }).start();
-    setModalActive(true);
-  };
-
-  const handleCloseModal = () => {
-    Animated.timing(fadeAnim, {
-      toValue: 0,
-      duration: 500,
-      useNativeDriver: true,
-    }).start();
-    setTimeout(() => {
-      setModalActive(false);
-    }, 500);
-  };
-
-  // get club id
-  const getClubId = (id) => {
-    setClubId(id);
-  };
-
-  // get club name
-  const getClubName = (name) => {
-    setClubName(name);
-  };
-
-  // this function allow to add or delete club from API depends what attribute were pass in setOption
-  const useFavoriteOptions = async (setOption, cardId) => {
-    const id = Number.isInteger(cardId);
-    const option = typeof setOption === "string";
-    const cardDetails = new FormData();
-
-    cardDetails.append("session_key", user.data.results.session_key);
-    cardDetails.append("club_id", id ? cardId : null);
-
-    if (setOption === "remove") {
-      await axios
-        .post(
-          `https://korty.org/api/clubs/bookmark/${option ? setOption : null}`,
-          cardDetails
-        )
-        .catch((err) => {
-          console.error(err);
-        });
-
-      onRemove(cardId);
-    }
-  };
-
+const ClubItem = ({ nav, data }) => {
   return (
     <React.Fragment>
-      {/* this condition need to be here for button at the bottom */}
       {data === "" ? (
         <Text>Nie posiadasz ulubionych klubów</Text>
       ) : (
@@ -78,51 +19,26 @@ const ClubItem = ({ nav, data, onRemove, onAdd }) => {
           keyExtractor={(item) => item.id.toString()}
           data={data}
           renderItem={({ item }) => (
-            <ClubCard
-              nav={nav}
-              item={item}
-              key={item.id}
-              onRemove={onRemove}
-              onAdd={useFavoriteOptions}
-              onModal={handleOpenModal}
-              clubId={getClubId}
-              clubName={getClubName}
-              onAdd={onAdd}
-            />
+            <ClubCard nav={nav} item={item} key={item.id} />
           )}
         />
       )}
       <View style={styles.button__container}>
-        <View style={styles.button__add}>
-          <Ionicons
-            style={styles.button__icon}
-            onPress={() => {
-              nav.navigate("Lista klubów");
-              clubListScreenActive();
-            }}
-            name="ios-search"
-            size={28}
-            color="#fff"
-          />
-        </View>
-      </View>
-      {modalActive && (
-        <Animated.View
-          style={[
-            styles.alertModal,
-            {
-              opacity: fadeAnim,
-            },
-          ]}
+        <TouchableOpacity
+          onPress={() => {
+            nav.navigate("Lista klubów");
+          }}
         >
-          <ClubAlertModal
-            onDelete={useFavoriteOptions}
-            onClose={handleCloseModal}
-            clubId={clubId}
-            clubName={clubName}
-          />
-        </Animated.View>
-      )}
+          <View style={styles.button__add}>
+            <Ionicons
+              style={styles.button__icon}
+              name="ios-search"
+              size={28}
+              color="#fff"
+            />
+          </View>
+        </TouchableOpacity>
+      </View>
     </React.Fragment>
   );
 };
@@ -144,15 +60,5 @@ const styles = StyleSheet.create({
   },
   button__icon: {
     color: "#fff",
-  },
-  alertModal: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "rgba(87, 81, 81, 0.3)",
   },
 });
