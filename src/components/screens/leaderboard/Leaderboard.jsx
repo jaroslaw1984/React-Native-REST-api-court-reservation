7fc825a510efd62c9b10ac33e7c46075f8c83d3a
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-08-17 21:42:16 +0200

    Gameplay component has been added

---
+++ src/components/screens/leaderboard/Leaderboard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { WebView } from "react-native-webview";
+import { createStackNavigator } from "@react-navigation/stack";
+import TopMenu from "../userMenu/TopMenu";
+
+const Stack = createStackNavigator();
+
+const content = () => {
+  return <WebView source={{ uri: "https://korty.org/rozgrywki" }} />;
+};
+
+const Leaderboard = ({ nav }) => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="content"
+        component={content}
+        options={{
+          headerTitle: (props) => <TopMenu {...props} navigation={nav} />,
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default Leaderboard;
