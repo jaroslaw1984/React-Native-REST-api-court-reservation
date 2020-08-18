281ac77f36850bcf87381ab7fea6fd020fbee841
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-08-18 20:43:06 +0200

    Adding loading indicator to the componet

--- src/components/screens/leaderboard/Leaderboard.jsx
+++ src/components/screens/leaderboard/Leaderboard.jsx
@@ -1,12 +1,54 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { View, ActivityIndicator } from "react-native";
 import { WebView } from "react-native-webview";
 import { createStackNavigator } from "@react-navigation/stack";
 import TopMenu from "../userMenu/TopMenu";
+import { UserContext } from "../../context/UserProvider";
 
 const Stack = createStackNavigator();
 
 const content = () => {
-  return <WebView source={{ uri: "https://korty.org/rozgrywki" }} />;
+  const [loading, setLoading] = useState(true);
+  const { user } = useContext(UserContext);
+
+  const location = user.data.results.location.link;
+  const userSessionKey = user.data.results.session_key;
+
+  const hideSpinner = () => {
+    setLoading(false);
+  };
+  const showSpinner = () => {
+    setLoading(true);
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+      <WebView
+        source={{
+          uri: `https://korty.org/logowanie/mobile?page=/rankingi${location}&sid=${userSessionKey}`,
+        }}
+        onLoadStart={() => showSpinner()}
+        onLoad={() => hideSpinner()}
+      />
+      {loading && (
+        <View
+          style={{
+            flex: 1,
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            position: "absolute",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          size="large"
+        >
+          <ActivityIndicator size="large" />
+        </View>
+      )}
+    </View>
+  );
 };
 
 const Leaderboard = ({ nav }) => {
--- src/components/screens/reservations/Reservations.jsx
+++ src/components/screens/reservations/Reservations.jsx
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { WebView } from "react-native-webview";
 import { createStackNavigator } from "@react-navigation/stack";
 import TopMenu from "../userMenu/TopMenu";
+import { View, ActivityIndicator } from "react-native";
 
 const Stack = createStackNavigator();
 
 const content = () => {
-  return <WebView source={{ uri: "https://korty.org/moje-rezerwacje" }} />;
+  const [loading, setLoading] = useState(true);
+
+  const hideSpinner = () => {
+    setLoading(false);
+  };
+  const showSpinner = () => {
+    setLoading(true);
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+      <WebView
+        source={{ uri: "https://korty.org/moje-rezerwacje" }}
+        onLoadStart={() => showSpinner()}
+        onLoad={() => hideSpinner()}
+      />
+      {loading && (
+        <View
+          style={{
+            flex: 1,
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            position: "absolute",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          size="large"
+        >
+          <ActivityIndicator size="large" />
+        </View>
+      )}
+    </View>
+  );
 };
 
 const Reservations = ({ nav }) => {
