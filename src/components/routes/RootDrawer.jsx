e8eb0267b875e0f8cacee2dfa141faf4f06c4ca9
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-08-18 17:13:16 +0200

    Removed unused functions

--- src/components/routes/RootDrawer.jsx
+++ src/components/routes/RootDrawer.jsx
@@ -1,26 +1,6 @@
 import React from "react";
-import { Button, View } from "react-native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import DrawerContent from "../screens/drawerContent/DrawerContent";
-
-const HomeScreen = ({ navigation }) => {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button
-        onPress={() => navigation.navigate("Notifications")}
-        title="Go to notifications"
-      />
-    </View>
-  );
-};
-
-const NotificationsScreen = ({ navigation }) => {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
-    </View>
-  );
-};
 
 const Drawer = createDrawerNavigator();
 
@@ -29,9 +9,9 @@ const RootDrawer = ({ bottomStack }) => {
     <Drawer.Navigator
       initialRouteName="Home"
       drawerContent={(props) => <DrawerContent {...props} />}
+      drawerPosition="right"
     >
       <Drawer.Screen name="Home" component={bottomStack} />
-      <Drawer.Screen name="Notifications" component={NotificationsScreen} />
     </Drawer.Navigator>
   );
 };
