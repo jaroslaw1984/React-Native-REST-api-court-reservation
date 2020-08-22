40e0cef456763316c3cf26a921f7c5378263299e
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-08-22 14:03:32 +0200

    Customizing theme and passing as PaperProvider to all components

--- App.js
+++ App.js
@@ -3,12 +3,24 @@ import "react-native-gesture-handler";
 import { StyleSheet, SafeAreaView } from "react-native";
 import Routes from "./src/components/routes/Routes";
 import { UserProvider } from "./src/components/context/UserProvider";
+import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#2f89fc",
+  },
+};
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <UserProvider>
-        <Routes />
+        <PaperProvider theme={theme}>
+          <Routes />
+        </PaperProvider>
       </UserProvider>
     </SafeAreaView>
   );
@@ -18,8 +30,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    // alignItems: "center",
-    // justifyContent: "center",
   },
 });
 
