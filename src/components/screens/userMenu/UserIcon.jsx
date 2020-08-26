9c97a8295f0b40adedaae9399bb48548342c7b05
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-08-26 19:53:03 +0200

    Creating and adding user menu that will toggle drawer at header menu at every bottom screen

---
+++ src/components/screens/userMenu/UserIcon.jsx
@@ -0,0 +1,31 @@
+import React, { useContext } from "react";
+import { Platform } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { FontAwesome } from "@expo/vector-icons";
+import { UserContext } from "../../context/UserProvider";
+import { Avatar } from "react-native-paper";
+
+const UserIcon = ({ navigation }) => {
+  const { user } = useContext(UserContext);
+  const userIcon = user.data.results.logo_src;
+
+  return (
+    <TouchableOpacity
+      onPress={() => navigation.toggleDrawer()}
+      style={{ marginRight: 15 }}
+    >
+      {userIcon ? (
+        <Avatar.Image source={{ uri: userIcon }} size={53} />
+      ) : (
+        <FontAwesome
+          name="user-circle"
+          size={35}
+          color="#40514e"
+          style={Platform.OS === "ios" ? { marginRight: 10 } : null}
+        />
+      )}
+    </TouchableOpacity>
+  );
+};
+
+export default UserIcon;
