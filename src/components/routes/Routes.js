1ab67ad978b253457d7b1115ec77cb43e8ba0483
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-09-15 21:56:49 +0200

    The deprecated AsyncStorage method has been replaced with the @react-native-community module

--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
-import { AsyncStorage } from "react-native";
+import AsyncStorage from "@react-native-community/async-storage";
 import { UserContext } from "../context/UserProvider";
 import UserMenu from "../screens/userMenu/UserMenu";
 import StackNavigator from "../screens/stackNavigator/StackNavigator";
@@ -9,17 +9,19 @@ import StackNavigator from "../screens/stackNavigator/StackNavigator";
 const Routes = () => {
   const { user, login } = useContext(UserContext);
 
+  const getUser = async () => {
+    try {
+      const value = await AsyncStorage.getItem("user");
+      if (value !== null) {
+        login();
+      }
+    } catch (err) {
+      console.log(new Error(err));
+    }
+  };
+
   useEffect(() => {
-    // check if user is logged in or not
-    AsyncStorage.getItem("user")
-      .then((item) => {
-        if (item) {
-          login();
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    getUser();
   }, []);
 
   return (
