f46f4b4ea67b72ce182f845253bab580bd742dac
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-09-18 21:05:43 +0200

    Toast has been replace by Snackbar from react-native-paper

--- src/components/screens/clubs/Clubs.jsx
+++ src/components/screens/clubs/Clubs.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
-import { StyleSheet, ActivityIndicator, View } from "react-native";
+import { StyleSheet, ActivityIndicator, View, BackHandler } from "react-native";
+import { Snackbar } from "react-native-paper";
 import { UserContext } from "../../context/UserProvider";
 import ClubItem from "./ClubItem";
 
@@ -32,7 +33,31 @@ const useClubs = (url) => {
 
 const Clubs = ({ nav }) => {
   useClubs("https://korty.org/api/clubs/bookmark/show");
+
   const { userClubs, loading, handleRemoveClub } = useContext(UserContext);
+  const [count, setCount] = useState(0);
+  const [visible, setVisible] = useState(false);
+
+  const onDismissSnackBar = () => setVisible(false);
+
+  useEffect(() => {
+    if (Platform.OS === "ios") return;
+    const backAction = BackHandler.addEventListener("hardwareBackPress", () => {
+      setCount(count + 1);
+      if (count < 1) {
+        setVisible(true);
+        backAction.remove();
+        return true;
+      } else {
+        BackHandler.exitApp();
+        backAction.remove();
+        return true;
+      }
+    });
+    setTimeout(() => {
+      setCount(0);
+    }, 2000);
+  });
 
   return (
     <React.Fragment>
@@ -41,7 +66,17 @@ const Clubs = ({ nav }) => {
           <ActivityIndicator size="large" />
         </View>
       ) : (
-        <ClubItem nav={nav} data={userClubs} onRemove={handleRemoveClub} />
+        <View style={styles.container}>
+          <Snackbar
+            visible={visible}
+            onDismiss={onDismissSnackBar}
+            duration={2000}
+            style={styles.snackbar}
+          >
+            Naciśnij ponownie, aby zamknąć
+          </Snackbar>
+          <ClubItem nav={nav} data={userClubs} onRemove={handleRemoveClub} />
+        </View>
       )}
     </React.Fragment>
   );
@@ -50,8 +85,14 @@ const Clubs = ({ nav }) => {
 export default Clubs;
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   loading_Indicator: {
     flex: 1,
     justifyContent: "center",
+  },
+  snackbar: {
+    backgroundColor: "rgba(0,0,0,0.7)",
   },
 });
