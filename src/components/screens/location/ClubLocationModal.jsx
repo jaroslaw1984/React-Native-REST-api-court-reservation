0af50653943e275fede26d330be47b14871fc7e3
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-10-13 20:00:25 +0200

    Adding modal that allow to select name of province, city and district

--- src/components/screens/location/ClubLocationModal.jsx
+++ src/components/screens/location/ClubLocationModal.jsx
@@ -1,11 +1,21 @@
-import React, { useContext, useEffect, useState } from "react";
-import { View, StyleSheet, Text, Dimensions } from "react-native";
-import { Button, Avatar, Card, IconButton } from "react-native-paper";
-import { UserContext } from "../../context/UserProvider";
-import { globalStyles } from "../../../../styles/global";
-import axios from "axios";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  Dimensions,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { Avatar, Card, IconButton } from "react-native-paper";
 
-const ClubLocationModal = ({ onClose, title }) => {
+const ClubLocationModal = ({
+  onClose,
+  title,
+  data,
+  clearData,
+  handleSelectedId,
+}) => {
   const [selectedIcon, setSelectedIcon] = useState("map-marker-radius");
 
   const cardSubtitle = "wybierz z listy...";
@@ -24,6 +34,27 @@ const ClubLocationModal = ({ onClose, title }) => {
     iconName();
   }, []);
 
+  const Item = ({ name, onPress }) => (
+    // <View style={styles.item}>
+    //   <Text style={styles.title}>{name.toUpperCase()}</Text>
+    // </View>
+    <TouchableOpacity onPress={onPress} style={styles.item}>
+      <Text style={styles.title}>{name.toUpperCase()}</Text>
+    </TouchableOpacity>
+  );
+
+  const renderItem = ({ item }) => {
+    // const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
+    return (
+      <Item
+        name={item.name}
+        onPress={() => {
+          handleSelectedId(item.id.toString());
+          onClose();
+        }}
+      />
+    );
+  };
   return (
     <View style={styles.modal__containter}>
       <Card.Title
@@ -31,9 +62,24 @@ const ClubLocationModal = ({ onClose, title }) => {
         subtitle={cardSubtitle}
         left={(props) => <Avatar.Icon {...props} icon={selectedIcon} />}
         right={(props) => (
-          <IconButton {...props} icon="close" onPress={() => onClose()} />
+          <IconButton
+            {...props}
+            icon="close"
+            onPress={() => {
+              clearData();
+              onClose();
+            }}
+          />
         )}
       />
+      <View style={styles.list__container}>
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+          // extraData={selectedId}
+        />
+      </View>
     </View>
   );
 };
@@ -48,25 +94,21 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#fff",
     borderRadius: 10,
+    overflow: "hidden",
   },
-  modal__warning__icon: {
-    alignSelf: "center",
-    padding: 10,
-    marginLeft: 10,
-  },
-  modal__text: {
-    fontSize: 18,
-    textAlign: "center",
-    paddingHorizontal: 10,
-  },
-  innterText: {
-    fontWeight: "bold",
-  },
-  modal__navBtn: {
+  list__container: {
     flex: 1,
-    flexDirection: "row",
-    padding: 15,
-    justifyContent: "space-around",
-    alignItems: "flex-end",
+    width: 350,
+    // backgroundColor: "green",
+  },
+  item: {
+    alignItems: "center",
+    // backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 20,
   },
 });
